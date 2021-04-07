ba557fdaee8a3f0c8d47aab9a675208d05456f2f
Christian Vermehren <47556064+cvermehren@users.noreply.github.com>
2021-04-07 09:48:33 +0200

    Update test3.R

--- test3.R
+++ test3.R
@@ -4,15 +4,22 @@ library(data.table)
 
 f <- list.files(path = "C:/Users/cve/Downloads", pattern = "Dash.pgn", full.names = T )
 pgn <- read.pgn(f, stat.moves = FALSE, extract.moves = 100)
+
 pgn <- pgn$Movetext[1]
 
-"1.e4 e5 2.Nf3 Nc6 3.Bb5 f5 4.Qe2 fxe4 5.Bxc6 dxc6 6.Qxe4 Bd6 7.Nxe5 Nf6 8.Qe2 O-O 9.d4 Re8 10.Be3 Bxe5 11.dxe5 Rxe5"
+inp <- gsub("[^[:alnum:]. ]", "", pgn)
+
+inp <- gsub("[^\\w\\-\\s:alnum:]|\\d", "", pgn, perl = TRUE)
+
+
+
+uci <- san2lan(inp)
+uci <- strsplit(uci, " ")[[1]]
+
 
 san2lan("1.e4 e5 2.Nf3 Nc6 3.Bb5 f5 4.Qe2 fxe4 5.Bxc6 dxc6 6.Qxe4 Bd6 7.Nxe5 Nf6 8.Qe2 O-O 9.d4 Re8 10.Be3 Bxe5 11.dxe5 Rxe5")
 
-
-
-dt <- extract_moves(pgn)
+san2lan("1.e4 e5 2.Nf3 Nc6 3.Bb5 f5 4.Qe2 fxe4 5.Bxc6 dxc6 6.Qxe4 Bd6 7.Nxe5 Nf6 8.Qe2 OO 9.d4 Re8 10.Be3 Bxe5 11.dxe5 Rxe5")
 
 endpoint <- "https://explorer.lichess.ovh/master?fen="
 startingpos <- "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
