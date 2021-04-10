66b60d9a8a3dd225145723c28aa7ee8a6cf02a60
Christian Vermehren <47556064+cvermehren@users.noreply.github.com>
2021-04-10 18:59:22 +0200

    Update test3.R

--- test3.R
+++ test3.R
@@ -1,4 +1,4 @@
-install.packages("bigchess")
+# install.packages("bigchess")
 
 library(bigchess)
 library(httr)
@@ -16,22 +16,55 @@ pgn[, moves_prun := gsub('\\-$', '', moves_prun)]
 san2vec <- Vectorize(san2lan)
 pgn[, uci := san2vec(moves_prun)]
 
+# Extract uci moves
 uci <- pgn[, list(uci)]
+line <- uci$uci[8]
 
-move <- uci$uci[1]
+# Cumulative paste
 
-chess_fun <- function(move) {
+
+chess_fun <- function(line) {
+  
+  line_frags <- strsplit(line, " ")[[1]]
+  
+  cumpaste = function(x, .sep = ",") Reduce(function(x1, x2) paste(x1, x2, sep = .sep), x, accumulate = TRUE)
+  
+  line_frags <- cumpaste(line_frags)
   
   endpoint <- "https://explorer.lichess.ovh/master?fen="
   startingpos <- "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+  
   url <- paste0(endpoint, startingpos)
   
-  url_move <- URLencode(paste0(url, "&play=", move) )
+  # total_games <- URLencode(url)
+  # total_games <- httr::GET(total_games)
+  # total_games <- httr::content(total_games)
+  # total_games <- total_games$white + total_games$draws + total_games$black
   
-  dt <- httr::GET(url_move)
-  ct <- httr::content(dt)
+  mylist <- list()
   
+  for (i in line_frags) {
+    
+    # <- line_frags[3]
+    url_move <- URLencode(paste0(url, "&play=", i) )
+    n_move <- httr::GET(url_move)
+    n_move <- httr::content(n_move)
+    n_move <- n_move$white + n_move$draws + n_move$black
+    #p_move <- n_move/total_games
+    
+    mylist[[i]] <- n_move
+    print(paste("Progress:", round(nchar(i)/max(nchar(line_frags))*100), "%" ))
+    Sys.sleep(2)
+    #print("Waking up")
+    }
+
+
+  stats <- lapply(mylist, data.table)
+  stats <- rbindlist(stats, idcol = T)
   
+  setnames(stats, names(stats), c("line", "frequency"))
+  stats[, frequency := as.numeric(frequency)]
+  stats[, cum_stats := round(frequency/max(frequency), 2)]
   
 }
 
