6c5cec5691131dc533a72aceff5bfc6ae95e47ad
Christian Vermehren <47556064+cvermehren@users.noreply.github.com>
2021-04-10 15:44:15 +0200

    Update test3.R

--- test3.R
+++ test3.R
@@ -1,24 +1,37 @@
+install.packages("bigchess")
+
 library(bigchess)
 library(httr)
 library(data.table)
 
-f <- list.files(path = "C:/Users/cve/Downloads", pattern = "Dash.pgn", full.names = T )
-pgn <- read.pgn(f, stat.moves = FALSE, extract.moves = 100)
+# PC: f <- list.files(path = "C:/Users/cve/Downloads", pattern = "Dash.pgn", full.names = T )
+f <- list.files(path = "/Users/cvermehren/Downloads/", pattern = "games.pgn", full.names = T)
+pgn <- data.table(read.pgn(f, stat.moves = FALSE, extract.moves = 100))
 
-pgn <- pgn$Movetext[1]
+# Keep only alphanumeric, space, dash (0-0) - then remove trailing dash
+pgn[, moves_prun := gsub("[^[:alnum:][:space:]\\. \\-]", "", Movetext)]
+pgn[, moves_prun := gsub('\\-$', '', moves_prun)]
 
-inp <- gsub("[^[:alnum:][:space:]\\. \\-]", "", pgn)
+# Vectorize fun and implement
+san2vec <- Vectorize(san2lan)
+pgn[, uci := san2vec(moves_prun)]
 
-uci <- san2lan(inp)
-uci <- strsplit(uci, " ")[[1]]
+uci <- pgn[, list(uci)]
 
-endpoint <- "https://explorer.lichess.ovh/master?fen="
-startingpos <- "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
-url <- paste0(endpoint, startingpos)
+move <- uci$uci[1]
 
-w_move <- paste0(uci[1], ",", uci[2], ",", uci[3])
+chess_fun <- function(move) {
+  
+  endpoint <- "https://explorer.lichess.ovh/master?fen="
+  startingpos <- "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+  url <- paste0(endpoint, startingpos)
+  
+  url_move <- URLencode(paste0(url, "&play=", move) )
+  
+  dt <- httr::GET(url_move)
+  ct <- httr::content(dt)
+  
+  
+  
+}
 
-url_move <- URLencode(paste0(url, "&play=", w_move) )
-
-dt <- httr::GET(url_move)
-ct <- httr::content(dt)
