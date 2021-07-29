fefa55e6dd68376a47c2d151e4391771f8cb43f9
Christian Vermehren <47556064+cvermehren@users.noreply.github.com>
2021-07-29 12:40:15 +0200

    Update current.R

--- current.R
+++ current.R
@@ -5,9 +5,6 @@ library(httr)
 library(data.table)
 library(stringr)
 
-
-# Get PGN file ------------------------------------------------------------
-
 # Guide: 
 # 1. Export opening lines as PGN from Chessbase, 
 # 2. import into Chessable, 
@@ -15,20 +12,18 @@ library(stringr)
 # 4. upload to R working directory
 
 
-# From Windows PC: 
-f <- list.files(path = "C:/Users/cve/Downloads", pattern = "FourKnights.pgn", full.names = T )
+# Settings ----------------------------------------------------------------
 
-# From Mac
-# f <- list.files(path = "/Users/cvermehren/Downloads/", pattern = "FourKnights.pgn", full.names = T)
+opening_name <- "4Knights-White"
+defining_line <- "1.e4 e5 2.Nf3 Nc6 3.Nc3 Nf6"
+pgn_file <- "C:/Users/cve/Downloads/FourKnights.pgn"
 
-pgn <- data.table(read.pgn(f, stat.moves = FALSE, extract.moves = 200))
+pgn <- data.table(read.pgn(pgn_file, stat.moves = FALSE, extract.moves = 0))
 
 
+# Cleansing ---------------------------------------------------------------
 
-# Clean PGNs --------------------------------------------------------------
-
-# Remove characters not within the range of x20 (SPACE) and x7E (~):
-# https://stackoverflow.com/questions/38828620/how-to-remove-strange-characters-using-gsub-in-r
+# Remove characters not within the range of x20 (SPACE) and x7E (~): https://stackoverflow.com/questions/38828620/how-to-remove-strange-characters-using-gsub-in-r
 pgn[, moves_prun := gsub('[^\x20-\x7E]', '', Movetext)]
 
 # Remove trailing dash
@@ -37,94 +32,81 @@ pgn[, moves_prun := gsub('\\-$', '', moves_prun)]
 # Remove trailing equal sign
 pgn[, moves_prun := gsub('\\=$', '', moves_prun)]
 
-# Remove leading & trailing whitespace, then remove double space
-# https://stackoverflow.com/questions/25707647/merge-multiple-spaces-to-single-space-remove-trailing-leading-spaces
+# Remove leading & trailing whitespace, then remove double space: https://stackoverflow.com/questions/25707647/merge-multiple-spaces-to-single-space-remove-trailing-leading-spaces
 pgn[, moves_prun := gsub("\\s+", " ", str_trim(moves_prun))]
 
-# Vectorize and apply san2lan
+# Convert to UCI notation (engines)
 san2vec <- Vectorize(san2lan)
 pgn[, uci := san2vec(moves_prun)]
 
-# Extract uci moves
-uci <- pgn[, list(uci)]
 
+# Functions ---------------------------------------------------------------
 
-# New ---------------------------------------------------------------------
-
-# Set defining Four Knights line: https://en.wikipedia.org/wiki/Four_Knights_Game
-fourknights <- "1.e4 e5 2.Nf3 Nc6 3.Nc3 Nf6"
-
-# line = uci$uci[1]
-# opening_line = "1.e4 e5 2.Nf3 Nc6 3.Nc3 Nf6"
-
-
-line_tree <- function(line, opening_line) {
+get_variation_levels <- function(variation, defining_line) {
   
-  opening <- san2lan(opening_line)
-  opening <- strsplit(opening, " ")[[1]]
-  opening <- paste(opening, collapse = ",")
+  defining_line_uci <- san2lan(defining_line)
+  defining_line_uci <- strsplit(defining_line_uci, " ")[[1]]
+  defining_line_uci <- paste(defining_line_uci, collapse = ",")
   
-  line_frags <- strsplit(line, " ")[[1]]
+  cumpaste_fun = function(x, .sep = ",") Reduce(function(x1, x2) paste(x1, x2, sep = .sep), x, accumulate = TRUE)
   
-  cumpaste = function(x, .sep = ",") Reduce(function(x1, x2) paste(x1, x2, sep = .sep), x, accumulate = TRUE)
+  variation_levels <- strsplit(variation, " ")[[1]]
+  variation_levels <- cumpaste_fun(variation_levels)
+  variation_levels <- variation_levels[nchar(variation_levels) >= nchar(defining_line_uci)]
   
-  line_frags <- cumpaste(line_frags)
-  
-  line_frags <- line_frags[nchar(line_frags) >= nchar(opening)]
-  
-  
-  return(line_frags)
+  return(variation_levels)
   
 }
 
-line <- lapply(uci$uci, line_tree, opening_line = fourknights)
-dt <- lapply(line, data.table)
-dt <- rbindlist(dt, idcol = T)
-dt <- dt[!duplicated(V1)]
 
-dt_list <- split(dt, by = ".id")
 
-# line <- data.table(unlist(line))
-# line <- unique(line)
-# line <- line$V1
 
-# dt$V1
-# line = dt[1:10]$V1
-# opening_line = fourknights
+# Create variation levels -------------------------------------------------
 
-opening_line <- fourknights
+variation_levels <- lapply(pgn$uci, get_variation_levels, defining_line = defining_line)
+variation_levels <- lapply(variation_levels, data.table)
+variation_levels <- rbindlist(variation_levels, idcol = T)
 
-opening <- san2lan(opening_line)
-opening <- strsplit(opening, " ")[[1]]
-opening <- paste(opening, collapse = ",")
+setnames(variation_levels, c(".id", "V1"), c("line", "variation_level"))
 
+variation_levels <- variation_levels[!duplicated(variation_level)]
+variation_levels <- split(variation_levels, by = "line")
+
+
+# Lichess API settings ----------------------------------------------------
 
 endpoint <- "https://explorer.lichess.ovh/master?fen="
 startingpos <- "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 
 url <- paste0(endpoint, startingpos)
 
-url_total_games <- URLencode(paste0(url, "&play=", opening) )
+
+# Total opening games -----------------------------------------------------
+
+url_total_games <- san2lan(defining_line)
+url_total_games <- strsplit(url_total_games, " ")[[1]]
+url_total_games <- paste(url_total_games, collapse = ",")
+url_total_games <- URLencode(paste0(url, "&play=", url_total_games) )
+
 total_games <- URLencode(url_total_games)
 total_games <- httr::GET(total_games)
 total_games <- httr::content(total_games)
 total_games <- total_games$white + total_games$draws + total_games$black
 
 
+# Extract statistics ------------------------------------------------------
 
-# n_line <- length(line$V1)
+print("Extracting move statistics from lichess.org master games collection")
 
-mylist <- list()
+stats_list <- list()
 
-#i=1
-# j = frag[1]
+pb <- txtProgressBar(min = 0, max = length(variation_levels), style = 3)
 
-
-for(i in seq_along(dt_list) ) {
+for(i in seq_along(variation_levels)) {
   
-  frag <- dt_list[[i]]$V1
+  var_levels <- variation_levels[[i]]$variation_level
   
-  for (j in frag) {
+  for (j in var_levels) {
     
     url_move <- URLencode(paste0(url, "&play=", j) )
     n_move <- httr::GET(url_move)
@@ -133,26 +115,29 @@ for(i in seq_along(dt_list) ) {
     
     if(exists("n_move")){ if(n_move / total_games < 0.01) break } 
     
-    mylist[[j]] <- n_move
+    stats_list[[j]] <- n_move
     
-  }
+    }
 
-
-    
-  print(paste("Progress:", round(i/length(dt_list) * 100), "%" ))
+  setTxtProgressBar(pb, i)
   
   Sys.sleep(2)
     
   }
  
-res <- data.table( line = names(res), frequency = unlist(mylist))
+res <- data.table( line = names(stats_list), frequency = unlist(stats_list))
 
-res[, rel_freq := frequency / shift(frequency, type = "lag")]
+res[startsWith(line, shift(line, type = "lag")) , line_no := 0 ]
+res[is.na(line_no) , line_no := 1 ]
+res[, line_no := cumsum(line_no) ]
 
-res[is.na(rel_freq), rel_freq := 1]
-res[, cumprod := round(cumprod(rel_freq)*100, 2)]
 
-keep <- res[cumprod >= 1]
+res[, rel_freq := frequency / shift(frequency, type = "lag"), by = line_no]
+res[is.na(rel_freq), rel_freq := frequency/max(frequency)]
+
+res[, cumprod := round(cumprod(rel_freq)*100, 2), by = line_no]
+
+keep <- res[cumprod > 1]
 keep[ startsWith( shift(line, type = "lead"), line), keep := FALSE]
 keep[is.na(keep), keep := TRUE]
 keep <- keep[keep == "TRUE"]
